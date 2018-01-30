a956737fa323f113c6c33fbdddb91f9f1045cbe6
Dan Marrinan <dan.marrinan@gmail.com>
2018-01-29 19:59:31 -0600

    displays todoText and indicates if todo is completed. also alerts user if todo list is empty.

---
+++ todosV5/todosV5.js
@@ -0,0 +1,52 @@
+//create a todoList object with data and methods
+var todoList = {
+    //store todos in an array of objects
+    todos : [],
+
+    //displays todos and indicates if they are completed or indicates that todo list is empty
+    displayTodos: function(){
+        if(this.todos.length === 0){
+            console.log('Your todo list is empty!')
+        }
+        else{
+            console.log('My Todos: ')
+            for (var i=0; i<this.todos.length; i++){
+                if(this.todos[i].completed === true){
+                    console.log('(x)', this.todos[i].todoText);
+                }
+                else{
+                    console.log('( )', this.todos[i].todoText);
+                }
+            }
+        }
+    },
+    
+    //add a todo with specified todoText
+    //initialize completed boolean to false
+    addTodo: function(todoText){
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
+        this.displayTodos();
+    },
+    
+    //change the value of todoText at a given position
+    changeTodo: function(position, todoText){
+        this.todos[position].todoText = todoText;
+        this.displayTodos();
+    },
+    
+    //remove a todo at a given posiition
+    deleteTodo: function(position){
+        this.todos.splice(position,1);
+        this.displayTodos();
+    },
+
+    //change completed boolean at a given position
+    toggleCompleted: function(position){
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
+    }
+}; 
